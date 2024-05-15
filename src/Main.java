fdfdb6466e711195d169f8b76c8c369ad2f48b41
Ibaqu <aquib.zt@gmail.com>
2024-05-15 11:18:06 -0400

    Add some basic auth

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
+
+    private static Map<String, String> users = new HashMap<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+       users.put("admin", "password");
+
+       Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter username : ");
+        String username = scanner.nextLine();
+
+        System.out.println("Please enter password : ");
+        String password = scanner.nextLine();
+
+        if (authenticate(username, password)) {
+            System.out.println("Authorized user detected");
+        } else {
+            System.out.println("Unauthorized user");
+        }
+    }
+
+    public static boolean authenticate(String username, String password) {
+        // Check username and password
+        return users.containsKey(username) && users.get(username).equals(password);
     }
 }
